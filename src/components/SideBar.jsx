0e5c2c62a44b9ce8400c168a075b9f5c1324176c
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-07-27 10:55:32 +1000

    style footer and main

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,13 +1,16 @@
 export default function SideBar() {
   return (
-    <div>
-      <h2>The Brutal Martial landscape</h2>
-      <div>
-        <p>Description</p>
-        <p>
-          dhfuhdf usdhf ushdfvksld vhsleihgfowshrgvuhsfdlvhsrgohd wsiedhfgo ls
-          ohws oghos hgoshoughv osh
-        </p>
+    <div className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <h2>The Brutal Martial landscape</h2>
+        <div>
+          <p>Description</p>
+          <p>
+            dhfuhdf usdhf ushdfvksld vhsleihgfowshrgvuhsfdlvhsrgohd wsiedhfgo ls
+            ohws oghos hgoshoughv osh
+          </p>
+        </div>
       </div>
     </div>
   );
