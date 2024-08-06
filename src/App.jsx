8daf4f21d6a3d36c9049971ba7ffc2d1b822e19b
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-08-06 12:01:42 +1000

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,23 @@ function App() {
     async function fetchAPIData(){
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
       const url = "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`
+
+      const today = (new Date()).toDateString()
+      const localkey = `NASA-${today}`
+      if(localStorage.getItem(localkey)){
+        const apiData = JSON.parse(localStorage.getItem(localkey))
+        setData(apiData)
+        console.log("Fetched from cache today")
+        return
+      }
+      localStorage.clear()
+
       try{
         const res = await fetch(url)
         const apiData = await res.json()
+        localStorage.setItem(localkey, JSON.stringify(apiData))
         setData(apiData)
-        console.log('DATA\n', apiData)
+        console.log('Fetched from API today')
       }catch{
         console.log(err.message)
       }
